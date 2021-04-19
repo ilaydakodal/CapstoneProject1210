e011afedb208f1090b956e7589f1ccec69f6ee7c
ilaydaKodal <45633668+ilaydakodal@users.noreply.github.com>
2021-04-19 11:40:56 +0300

    Add SymptomTestViewController

---
+++ Project1210/Swift/Features/Main/Model/SymptomQuestionModel.swift
@@ -0,0 +1,15 @@
+//
+//  SymptomQuestionModel.swift
+//  Project1210
+//
+//  Created by Ilayda Kodal on 4/18/21.
+//
+
+import Foundation
+
+struct Question {
+    var question: String!
+    var answerA: String!
+    var answerB: String!
+}
+
