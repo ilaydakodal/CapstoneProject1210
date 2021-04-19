ec1063d5293fcfb8c036668211618a387a450e28
ilaydaKodal <45633668+ilaydakodal@users.noreply.github.com>
2021-04-20 00:58:08 +0300

    Add pin button action

--- Project1210/Swift/Features/MapView/MapViewController.swift
+++ Project1210/Swift/Features/MapView/MapViewController.swift
@@ -14,12 +14,12 @@ class MapViewController: UIViewController {
     
     lazy var userProfile = UserProfileViewController()
     lazy var symptomTestView = SymptomTestViewController()
-    
+    var manager = LocationManager()
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        LocationManager.shared.getUserLocation { [weak self] location in
+       LocationManager.shared.getUserLocation { [weak self] location in
             DispatchQueue.main.async {
                 guard let strongSelf = self else {
                     return
@@ -36,8 +36,8 @@ class MapViewController: UIViewController {
     }
     
     @IBAction func PinButtonPressed(_ sender: Any) {
-        mapView.showsUserLocation = true
-        
+       relocate()
+    
         print("location")
     }
     
@@ -49,9 +49,23 @@ class MapViewController: UIViewController {
         let pin = MKPointAnnotation()
         pin.coordinate = location.coordinate
         mapView.setRegion(MKCoordinateRegion(center: location.coordinate,
-                                             span: MKCoordinateSpan(latitudeDelta: 0.1,
-                                                                    longitudeDelta: 0.1)),
+                                             span: MKCoordinateSpan(latitudeDelta: 0.05,
+                                                                    longitudeDelta: 0.05)),
                           animated: true)
-        mapView.addAnnotation(pin)
+       
+        //mapView.addAnnotation(pin)
+    }
+    func relocate() {
+        mapView.setVisibleMapRect(
+            mapView.annotations.reduce(MKMapRect.null) { result, next in
+                let point = MKMapPoint(next.coordinate)
+                let rect = MKMapRect(x: point.x, y: point.y, width: 0, height: 0)
+                guard !result.isNull else { return rect }
+                return result.union(rect)
+            },
+            edgePadding: UIEdgeInsets(top: 20, left: 20, bottom: 40, right: 20),
+            animated: true
+        )
+        //manager.manager.startUpdatingLocation()
     }
 }
